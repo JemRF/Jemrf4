59845e56015bf9d975af53c2724ea2fb320f512f
Glenn Seaton <39573756+JemRF@users.noreply.github.com>
2025-12-14 23:27:51 -0600

    Add files via upload

---
+++ usb0/serial_mon.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Updated to python 3
+import sys
+from threading import Thread
+#from bme280 import process_bme_reading
+from rflib import rf2serial, fetch_messages, request_reply
+import rflib
+from time import sleep
+import time
+import sys, os
+
+def inbound_message_processing():
+  try:
+    while (True):
+        sleep(0.2)
+        fetch_messages(0)
+        while len(rflib.processing_queue)>0:
+            message = rflib.processing_queue.pop(0)
+            addspace = ""
+            if len(message[0]) <= 2:
+                addspace = "  "
+            print(time.strftime("%c")+" "+addspace+message[0]+" "+message[1])
+        if rflib.event.is_set():
+            break
+  except Exception as e:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(e).__name__, e.args)
+        print (message)
+        print (e)
+        rflib.event.set()
+        exit()
+
+def main():
+  print ("JemRF Serial Monitor 2.3.2-R4")
+  print ("Press ctrl-c to exit")
+
+  rflib.init()
+  #start serial processing thread
+  a=Thread(target=rf2serial, args=())
+  a.start()
+  sleep(1)  # wait for serial thread to start
+  #now start processing thread
+  print(f'Receiver started {time.strftime("%c")}')
+  b=Thread(target=inbound_message_processing, args=())
+  b.start()
+
+  while not rflib.event.is_set():
+      try:
+          sleep(1)
+      except KeyboardInterrupt:
+          rflib.event.set()
+          break
+  print("End of Event")
+  print(rflib.event.is_set())
+
+if __name__ == "__main__":
+    try:
+      main()
+    except Exception as e:
+      template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+      message = template.format(type(e).__name__, e.args)
+      exc_type, exc_obj, exc_tb = sys.exc_info()
+      fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+      message += exe_tb.tb_lineno
+      print (message)
+      print (e)
+      rflib.event.set()
+    finally:
+      rflib.event.set()
+      exit()
